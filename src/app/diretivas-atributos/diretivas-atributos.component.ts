7c71f9fe1ecb7570a730e4168104330e631b4f8f
Eliel Lopes Dos Santos <ellsantos@rd.com.br>
2023-01-18 09:21:27 -0300

    ngModel

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -11,6 +11,9 @@ export class DiretivasAtributosComponent implements OnInit {
   public altura: string = "20px";
   public backgroundColor: string = "red";
 
+  public nome: string = "";
+  public list: Array<{ nome: string }> = [];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -31,6 +34,10 @@ export class DiretivasAtributosComponent implements OnInit {
       }
     }, 2000);
 
+  }
+
+  public salvar () {
+    this.list.push({ nome: this.nome });
   }
 
 }
